0832cd486e19dd44caec1bff575035e18914aea2
Edgar Gutierrez <edgar3433gm@gmail.com>
2024-07-09 11:11:43 -0500

    actividades con print e input

---
+++ 3_Input.py
@@ -0,0 +1,11 @@
+#print("Dime tu nombre  ")
+# input("Dime tu nombre: ")
+# input("Digita tu apellido")
+
+#print(input("Dime tu nombre: "))
+#print("Tu nombre es " + input("Escribe tu nombre: "))
+
+#print("Tu estudias: "+ input("Qué estás estudiando? "))
+#print("El pais donde resides es: " + input("¿En qué país vives?")) 
+
+print("Nomnbre de usuario: " + input("Digita tus apellidos: ") + " " + input("Digita tus nombres "  ))
---
+++ INTRODUCCION/2_strings.py
@@ -0,0 +1,19 @@
+# print("hola mundo 2")
+# print(150 +5)
+# print("150 + 5")
+# print("edgar")
+# print("Gutierrez")
+# print('Edgar ' + 'Gutierrez')
+# print('Edgar'+' '+'Gutierrez')
+
+
+#print("me llamo \"Edgar\"")
+#print("esta es una lines \ny esta es otra linea")
+#print("\testa es la tercera linea")
+#print("\tThis isn\'t a number")
+
+print("Linea 1\nLínea 2\nLinea 3")
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+
+print("practica dos martes clase 4")
+print("\n Un espacio interlineado")
